6761e8f655cd4d2d280ffbf28f6e52dc659579cf
vakiazaraiah <azaraiah.vaki@my.jcu.edu.au>
2018-08-15 19:36:14 +1000

    Add password program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 """Azaraiah Vaki"""
+password = input("Enter password// ")
+
+while len(password) < 4:
+    print("Invalid passeword")
+    password = input("Enter password// ")
+
+for char in password:
+    print("*", end='')
